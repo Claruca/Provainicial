992fdae24751d0be9fcb450d379b805737d3a944
Claruca <32461296+Claruca@users.noreply.github.com>
2017-10-02 18:44:08 +0200

    comentari
    afegim comentari

--- src/Factorial.java
+++ src/Factorial.java
@@ -3,7 +3,7 @@ public class Factorial {
     public static void main(String[] args) {
         System.out.println(factorial(5));
     }
-
+// som un comentari
     public static int factorial(int n) {
         int resultado = 1;
         for (int i = 1; i <= n; i++) {
